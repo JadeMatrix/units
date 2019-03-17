3827454a47493d6c1a0ad0d1307622337c9dd966
Joseph Durel <jadematrix.art@gmail.com>
2019-03-16 23:18:43 -0400

    Replaced initial basic unit tests with per-system conversion coverage tests

---
+++ tests/unit_tests/linear_imperial.cpp
@@ -0,0 +1,39 @@
+#include <main.hpp>
+#include <units/linear.hpp>
+
+namespace units = ::JadeMatrix::units;
+
+void test()
+{
+    {
+        units::feet< double > feet{ 7 };
+        units::inches< double > inches{ feet };
+        CHECK( static_cast< double >( inches ) == 84.0 );
+    }
+    {
+        units::yards< double > yards{ 7 };
+        units::feet< double > feet{ yards };
+        CHECK( static_cast< double >( feet ) == 21.0 );
+    }
+    {
+        units::yards< double > yards{ 7 };
+        units::inches< double > inches{ yards };
+        CHECK( static_cast< double >( inches ) == 252.0 );
+    }
+    
+    {
+        units::inches< double > inches{ 7 };
+        units::feet< double > feet{ inches };
+        CHECK( static_cast< double >( feet ) == 7.0 / 12.0 );
+    }
+    {
+        units::feet< double > feet{ 7 };
+        units::yards< double > yards{ feet };
+        CHECK( static_cast< double >( yards ) == 7.0 / 3.0 );
+    }
+    {
+        units::inches< double > inches{ 7 };
+        units::yards< double > yards{ inches };
+        CHECK( static_cast< double >( yards ) == 7.0 / 36.0 );
+    }
+}
