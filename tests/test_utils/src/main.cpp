212f9353fd7c37692e81755396bbe4f6554886ee
Joseph Durel <jadematrix.art@gmail.com>
2019-03-16 22:03:25 -0400

    Added unit test utility to decompose & print values in test expressions, inspired by Catch2's `Decomposer` class

--- tests/test_utils/src/main.cpp
+++ tests/test_utils/src/main.cpp
@@ -1,7 +1,18 @@
 #include <main.hpp>
 
+#include <iostream>
+
 
 int main( int, char*[] )
 {
-    return test() ? 0 : 1;
+    try
+    {
+        test();
+        return 0;
+    }
+    catch( const ::tests::failure& f )
+    {
+        std::cout << f.what() << std::endl;
+        return 1;
+    }
 }
