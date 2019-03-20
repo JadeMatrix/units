e67177d2d0c91a7b620793a7a0dee6df302f95d5
Joseph Durel <jadematrix.art@gmail.com>
2019-03-19 23:34:51 -0400

    Rewrote `stream_format.hpp` to use `internal/traits_utils.hpp`

--- include/units/stream_format.hpp
+++ include/units/stream_format.hpp
@@ -5,58 +5,31 @@
 
 #include "strings.hpp"
 #include "internal/core_types.hpp"
+#include "internal/traits_utils.hpp"
 
 #include <ostream>
 
 
 namespace JadeMatrix { namespace units // Stream formatting operators //////////
 {
-    template< typename Traits, typename Scale, typename T >
-    std::ostream& operator<<(
+    template< typename Unit > auto operator<<(
         std::ostream& out,
-        const unit< Traits, Scale, T >& u
-    )
+        const Unit& u
+    ) -> typename std::enable_if<
+        ( is_unit< Unit >::value && !is_ratio< Unit >::value ),
+        std::ostream&
+    >::type
     {
         return out
-            << static_cast< T >( u )
-            << unit_strings< unit< Traits, Scale, T > >::symbol()
+            << static_cast< typename Unit::value_type >( u )
+            << unit_strings< Unit >::symbol()
         ;
     }
     
-    template<
-        template< typename > class Numer_Unit,
-        template< typename > class Denom_Unit,
-        typename T
-    >
-    std::ostream& operator<<(
+    template< typename T > std::ostream& operator<<(
         std::ostream& out,
-        const per< Numer_Unit, Denom_Unit, T >& u
+        const ratio< T >& u
     )
-    {
-        return out
-            << static_cast< T >( u )
-            << unit_strings< per< Numer_Unit, Denom_Unit, T > >::symbol()
-        ;
-    }
-    
-    template<
-        template< typename > class First_Unit,
-        template< typename > class Second_Unit,
-        typename T
-    >
-    std::ostream& operator<<(
-        std::ostream& out,
-        const by< First_Unit, Second_Unit, T >& u
-    )
-    {
-        return out
-            << static_cast< T >( u )
-            << unit_strings< by< First_Unit, Second_Unit, T > >::symbol()
-        ;
-    }
-    
-    template< typename T >
-    std::ostream& operator<<( std::ostream& out, const ratio< T >& u )
     {
         return out << static_cast< T >( u );
     }
