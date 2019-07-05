dac2aae85c46752d8c69cb991abfcc790a3d1c80
Joseph Durel <jadematrix.art@gmail.com>
2019-07-05 13:52:34 -0400

    Added more ratio unit tests

--- tests/compile/ratio_conversion.cpp
+++ tests/compile/ratio_conversion.cpp
@@ -7,17 +7,42 @@ namespace units = ::JadeMatrix::units;
 
 int main( int, char*[] )
 {
-    units::feet< double > f1{ 7 };
-    units::feet< double > f2{ 3 };
-    
-    auto ff = f1 / f2;
-    
-    units::radians< double > r1{ 7 };
-    units::radians< double > r2{ 3 };
-    
-    auto rr = r1 / r2;
-    
-    ff = rr;
+    {
+        units::feet< double > f1{ 7 };
+        units::feet< double > f2{ 3 };
+        
+        auto ff = f1 / f2;
+        
+        units::radians< double > r1{ 7 };
+        units::radians< double > r2{ 3 };
+        
+        auto rr = r1 / r2;
+        
+        ff = rr;
+    }
+    {
+        auto r1 = ( (
+              units::    meters< double >{ 3 }
+            / units::decimeters< double >{ 7 }
+        ) / (
+              units::kilometers< double >{  4 }
+            / units::    meters< double >{ 20 }
+        ) );
+        units::ratio< double > r2 = r1;
+        
+        auto r3 = ( (
+              units::nautical_miles< double >{ 3 }
+            / units::degrees       < double >{ 7 }
+        ) / (
+              units::  kiloyards< double >{  4 }
+            / units::revolutions< double >{ 20 }
+        ) );
+        units::ratio< double > r4 = r3;
+        
+        auto r5 = r1 / r3;
+        units::ratio< double > r6 = r5;
+        r6 = r2 / r4;
+    }
     
     return 0;
 }
---
+++ tests/suites/ratio.cpp
@@ -0,0 +1,59 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#include <units/angular.hpp>
+#include <units/linear.hpp>
+#include <units/temporal.hpp>
+#include <units/core/stream_format.hpp>
+
+
+namespace units = ::JadeMatrix::units;
+
+
+TEST_CASE( "preserve relative scales in ratio (larger numerator)" )
+{
+    auto r = (
+          units::kilometers< double >{ 1 }
+        / units::    meters< double >{ 1 }
+    );
+    REQUIRE( r == units::ratio< double >( 1000.0 ) );
+}
+
+TEST_CASE( "preserve relative scales in ratio (larger denominator)" )
+{
+    auto r = (
+          units::centihours< double >{ 1 }
+        / units::     hours< double >{ 1 }
+    );
+    REQUIRE( r == units::ratio< double >( 0.01 ) );
+}
+
+TEST_CASE(
+    "preserve relative scales in ratio (reduce to 1, convertible units)"
+)
+{
+    auto r1 = (
+          units::     degrees< double >{ 1 }
+        / units::millidegrees< double >{ 1 }
+    );
+    auto r2 = (
+          units::kilorevolutions< double >{ 1 }
+        / units::    revolutions< double >{ 1 }
+    );
+    REQUIRE( ( r1 / r2 ) == units::ratio< double >( 1.0 ) );
+}
+
+TEST_CASE(
+    "preserve relative scales in ratio (reduce to 1, non-convertible units)"
+)
+{
+    auto r1 = (
+          units::     radians< double >{ 1 }
+        / units::microradians< double >{ 1 }
+    );
+    auto r2 = (
+          units::megafeet< double >{ 1 }
+        / units::    feet< double >{ 1 }
+    );
+    REQUIRE( ( r1 / r2 ) == units::ratio< double >( 1.0 ) );
+}
