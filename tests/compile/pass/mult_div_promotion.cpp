c9bb9628982f5955062961de2f3e12402e3a1b4d
Joseph Durel <jadematrix.art@gmail.com>
2019-10-03 23:58:38 -0400

    Added compile test for multiplication & division by non-units

---
+++ tests/compile/pass/mult_div_promotion.cpp
@@ -0,0 +1,34 @@
+#include <units/linear.hpp>
+
+#include <type_traits>  // is_same
+
+
+namespace units = ::JadeMatrix::units;
+
+
+int main( int, char*[] )
+{
+    {
+        auto d = units::feet< double >{ 3 } * 7;
+        static_assert(
+            std::is_same< decltype( d ), units::feet< double > >::value,
+            "std::is_same< decltype( d ), nits::feet< double > >::value"
+        );
+    }
+    {
+        auto d = units::centimiles< short >{ 3 } * 7L;
+        static_assert(
+            std::is_same< decltype( d ), units::centimiles< long > >::value,
+            "std::is_same< decltype( d ), units::centimiles< long > >::value"
+        );
+    }
+    {
+        auto d = units::kilometers< char >{ 48 } / 6.0;
+        static_assert(
+            std::is_same< decltype( d ), units::kilometers< double > >::value,
+            "std::is_same< decltype( d ), nits::kilometers< double > >::value"
+        );
+    }
+    
+    return 0;
+}
