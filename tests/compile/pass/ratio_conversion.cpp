f65fd035b8eb0bcc5a669508fee6d6dba31e7c1e
Joseph Durel <jadematrix.art@gmail.com>
2019-10-06 21:49:59 -0400

    Added test cases to ratio conversion compile tests

--- tests/compile/pass/ratio_conversion.cpp
+++ tests/compile/pass/ratio_conversion.cpp
@@ -1,6 +1,8 @@
 #include <units/angular.hpp>
 #include <units/linear.hpp>
 
+#include <type_traits>  // is_same
+
 
 namespace units = ::JadeMatrix::units;
 
@@ -42,6 +44,42 @@ int main( int, char*[] )
         auto r5 = r1 / r3;
         units::ratio< double > r6 = r5;
         r6 = r2 / r4;
+    }
+    {
+        auto v = units::miles< int >( 7 ) * units::ratio< int >( 1 );
+        static_assert(
+            std::is_same< decltype( v ), units::miles< int > >::value,
+            "!std::is_same< decltype( v ), units::miles< int > >::value"
+        );
+    }
+    {
+        auto v = units::ratio< int >( 1 ) * units::meters< int >( 7 );
+        static_assert(
+            std::is_same< decltype( v ), units::meters< int > >::value,
+            "!std::is_same< decltype( v ), units::meters< int > >::value"
+        );
+    }
+    {
+        auto v = units::inches< int >( 7 ) / units::ratio< int >( 1 );
+        static_assert(
+            std::is_same< decltype( v ), units::inches< int > >::value,
+            "!std::is_same< decltype( v ), units::inches< int > >::value"
+        );
+    }
+    {
+        auto v = units::ratio< int >( 1 ) / units::feet< int >( 7 );
+        static_assert(
+            units::internal::is_same_unit<
+                decltype( v )::template unit_type,
+                units::per<
+                    units::ratio,
+                    units::feet,
+                    void
+                >::template unit_type
+            >::value,
+            "!std::is_same< decltype( v ), units::per< units::ratio, "
+            "units::feet, int > >::value"
+        );
     }
     
     return 0;
