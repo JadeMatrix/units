6c7cc6abe5e3032f5d9dfe7df647bf83710c1678
Joseph Durel <jadematrix.art@gmail.com>
2019-10-06 23:47:32 -0400

    Added initial digital units (note that `flops` and `ips` will be redefined as `per`s later)

---
+++ include/units/digital.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef JM_UNITS_DIGITAL_HPP
+#define JM_UNITS_DIGITAL_HPP
+
+
+// #include "core/constants.hpp"
+#include "core/define_unit.hpp"
+
+
+namespace JadeMatrix { namespace units // Declarations /////////////////////////
+{
+    /*
+    No automatic conversion is given between `bits` and `bytes` because:
+        - Not all computers use 8-bit bytes.  While most of the machines that
+          don't are only of historical interest, they still exist or did exist.
+          Besides that...
+        - There is no way of knowing the user's intention in using `bits`.  They
+          may be using it to store the compressed size of some data that may not
+          have a direct relationship to the bytes in which it is stored.  For
+          example, ASCII is a 7-bit character encoding commonly stored in 8-bit
+          bytes, with one bit unused.
+    */
+    
+    struct  bit_traits {};
+    struct byte_traits {};
+    struct flop_traits {};
+    struct   ip_traits {}; // Instructions-Per-Second, not Internet Protocol
+    
+    DEFINE_ALL_PREFIXES_FOR_UNIT(  bits,  bit_traits )
+    DEFINE_ALL_PREFIXES_FOR_UNIT( bytes, byte_traits )
+    DEFINE_ALL_PREFIXES_FOR_UNIT( flops, flop_traits )
+    DEFINE_ALL_PREFIXES_FOR_UNIT(   ips,   ip_traits )
+    
+    DEFINE_ALL_STRINGS_FOR_UNIT(  bit_traits,  "bits",     "b" )
+    DEFINE_ALL_STRINGS_FOR_UNIT( byte_traits, "bytes",     "B" )
+    DEFINE_ALL_STRINGS_FOR_UNIT( flop_traits, "FLOPS", "FLOPS" )
+    DEFINE_ALL_STRINGS_FOR_UNIT(   ip_traits,   "IPS",   "IPS" )
+} }
+
+
+#endif
