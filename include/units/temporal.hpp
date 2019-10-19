433403f06c36b6e831e9f3af670398ed4a507cd2
Joseph Durel <jadematrix.art@gmail.com>
2019-10-18 23:19:37 -0400

    Rewrote temporal units specializations supporting conversion to & from `std::chrono::duration`s

--- include/units/temporal.hpp
+++ include/units/temporal.hpp
@@ -7,9 +7,11 @@
 #include "core/define_unit.hpp"
 
 #include <chrono>
+#include <type_traits>  // enable_if, true_type, false_type
+#include <utility>      // declval
 
 
-namespace JadeMatrix { namespace units // Declarations /////////////////////////
+namespace JadeMatrix { namespace units
 {
     struct                  second_traits {};
     struct                  minute_traits {};
@@ -20,6 +22,104 @@ namespace JadeMatrix { namespace units // Declarations /////////////////////////
     // struct standard_gregorian_year_traits {};  // 365 days
     // struct           average_month_traits {};  //  30.436875 std. days
     // struct  average_gregorian_year_traits {};  // 365.2425   std. days
+} }
+
+
+namespace JadeMatrix { namespace units
+{
+    // Specialization of `unit<>` that supports conversion to & from
+    // `std::chrono::duration`s
+    
+    namespace internal
+    {
+        template<
+            typename T,
+            typename = void
+        > struct is_std_chrono_duration : std::false_type {};
+        template< typename T > struct is_std_chrono_duration<
+            T,
+            void_t< decltype(
+                std::chrono::duration_cast< T >( std::declval< T >() )
+            ) >
+        > : std::true_type {};
+    }
+    
+    #define DEFINE_UNIT_SPECIALIZATION_FOR( TRAITS, CHRONO_RATIO ) \
+    template< \
+        typename Scale, \
+        typename T \
+    > class unit< TRAITS, Scale, T > : \
+        public internal::unit_impl< TRAITS, Scale, T > \
+    { \
+    protected: \
+        using impl_type = internal::unit_impl< TRAITS, Scale, T >; \
+         \
+    public: \
+        using internal::unit_impl< TRAITS, Scale, T >::unit_impl; \
+         \
+        template< typename Rep, typename Period > unit( \
+            const std::chrono::duration< Rep, Period >& s \
+        ) : impl_type( std::chrono::duration< \
+            typename impl_type::value_type, \
+            std::ratio_multiply< \
+                CHRONO_RATIO, \
+                typename impl_type::scale_type \
+            > \
+        >( s ).count() ) \
+        {} \
+         \
+        template< \
+            typename O, \
+            typename = typename std::enable_if< \
+                   !internal::is_unit               < O >::value \
+                && !internal::is_std_chrono_duration< O >::value \
+            >::type \
+        > explicit constexpr operator O () const \
+        { \
+            return static_cast< O >( impl_type::_value ); \
+        } \
+         \
+        template< \
+            typename Rep, \
+            typename Period \
+        > operator std::chrono::duration< Rep, Period >() \
+        { \
+            return std::chrono::duration< \
+                typename impl_type::value_type, \
+                std::ratio_multiply< \
+                    CHRONO_RATIO, \
+                    typename impl_type::scale_type \
+                > \
+            >( impl_type::_value ); \
+        } \
+    };
+    
+    DEFINE_UNIT_SPECIALIZATION_FOR( second_traits, std::ratio<    1 > )
+    DEFINE_UNIT_SPECIALIZATION_FOR( minute_traits, std::ratio<   60 > )
+    DEFINE_UNIT_SPECIALIZATION_FOR(   hour_traits, std::ratio< 3600 > )
+    
+    #undef DEFINE_UNIT_SPECIALIZATION_FOR
+} }
+
+
+namespace JadeMatrix { namespace units
+{
+    #define DEFINE_PREFIX_FOR_seconds( PREFIX, SCALE ) template< typename T > using PREFIX##seconds = unit< second_traits, SCALE, T >;
+    #define DEFINE_PREFIX_FOR_minutes( PREFIX, SCALE ) template< typename T > using PREFIX##minutes = unit< minute_traits, SCALE, T >;
+    #define DEFINE_PREFIX_FOR_hours(   PREFIX, SCALE ) template< typename T > using PREFIX##hours   = unit<   hour_traits, SCALE, T >;
+    
+    JM_UNITS_FOREACH_SCALE( DEFINE_PREFIX_FOR_seconds )
+    JM_UNITS_FOREACH_SCALE( DEFINE_PREFIX_FOR_minutes )
+    JM_UNITS_FOREACH_SCALE( DEFINE_PREFIX_FOR_hours   )
+    
+    #undef DEFINE_PREFIX_FOR_seconds
+    #undef DEFINE_PREFIX_FOR_minutes
+    #undef DEFINE_PREFIX_FOR_hours
+    
+    
+    JM_UNITS_DEFINE_STRINGS_FOR_TRAITS( second_traits, "seconds", "s" )
+    JM_UNITS_DEFINE_STRINGS_FOR_TRAITS( minute_traits, "minutes", "m" )
+    JM_UNITS_DEFINE_STRINGS_FOR_TRAITS(   hour_traits, "hours"  , "h" )
 } }
 
 
@@ -67,148 +167,6 @@ namespace JadeMatrix { namespace units
         minute_traits&&,
         hour_traits&&
     );
-} }
-
-
-namespace JadeMatrix { namespace units
-{
-    // Create a subclass of `unit` to add implicit conversions to & from
-    // `std::chrono::duration<>` types
-    
-    #define JM_UNITS_INTERNAL_DEFINE_MOST_STRINGS_FOR_TEMPORAL_UNIT( \
-        UNIT_PLURAL, \
-        TRAITS, \
-        RATIO, \
-        UNIT_NAME_STR, \
-        UNIT_SYM_STR \
-    ) \
-     \
-    template< \
-        typename Scale, \
-        typename T = void \
-    > struct _##UNIT_PLURAL##_unit : public unit< \
-        TRAITS, \
-        Scale, \
-        T \
-    > \
-    { \
-    public: \
-        using unit< TRAITS, Scale, T >::unit; \
-         \
-        template< typename O > _##UNIT_PLURAL##_unit( \
-            const std::chrono::duration< O, RATIO >& s \
-        ) : \
-            unit< TRAITS, Scale, T >( s.count() ) \
-        {} \
-         \
-        template< typename O, typename R > _##UNIT_PLURAL##_unit( \
-            const std::chrono::duration< O, R >& o \
-        ) : \
-            unit< TRAITS, Scale, T >( std::chrono::duration_cast< \
-                std::chrono::duration< O > \
-            >( o ).count() ) \
-        {} \
-         \
-        template< \
-            typename Rep, \
-            typename Period \
-        > constexpr operator std::chrono::duration< Rep, Period > () const \
-        { \
-            return std::chrono::duration< Rep, Period >{ this -> _value }; \
-        } \
-    }; \
-     \
-    template< typename Scale > struct _##UNIT_PLURAL##_unit< \
-        Scale, \
-        void \
-    > : public unit< \
-        TRAITS, \
-        Scale, \
-        void \
-    > \
-    {}; \
-      \
-    template< typename T > using         UNIT_PLURAL = _##UNIT_PLURAL##_unit<  unit_scale, T >; \
-    template< typename T > using dozen_##UNIT_PLURAL = _##UNIT_PLURAL##_unit< dozen_scale, T >; \
-    template< typename T > using     bi##UNIT_PLURAL = _##UNIT_PLURAL##_unit<    bi_scale, T >; \
-    template< typename T > using   semi##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  semi_scale, T >; \
-     \
-    template< typename T > using    exa##UNIT_PLURAL = _##UNIT_PLURAL##_unit<   exa_scale, T >; \
-    template< typename T > using   peta##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  peta_scale, T >; \
-    template< typename T > using   tera##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  tera_scale, T >; \
-    template< typename T > using   giga##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  giga_scale, T >; \
-    template< typename T > using   mega##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  mega_scale, T >; \
-    template< typename T > using   kilo##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  kilo_scale, T >; \
-    template< typename T > using  hecto##UNIT_PLURAL = _##UNIT_PLURAL##_unit< hecto_scale, T >; \
-    template< typename T > using   deca##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  deca_scale, T >; \
-     \
-    template< typename T > using   deci##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  deci_scale, T >; \
-    template< typename T > using  centi##UNIT_PLURAL = _##UNIT_PLURAL##_unit< centi_scale, T >; \
-    template< typename T > using  milli##UNIT_PLURAL = _##UNIT_PLURAL##_unit< milli_scale, T >; \
-    template< typename T > using  micro##UNIT_PLURAL = _##UNIT_PLURAL##_unit< micro_scale, T >; \
-    template< typename T > using   nano##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  nano_scale, T >; \
-    template< typename T > using   pico##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  pico_scale, T >; \
-    template< typename T > using  femto##UNIT_PLURAL = _##UNIT_PLURAL##_unit< femto_scale, T >; \
-    template< typename T > using   atto##UNIT_PLURAL = _##UNIT_PLURAL##_unit<  atto_scale, T >; \
-     \
-    JM_UNITS_DEFINE_STRINGS_FOR_TRAITS( TRAITS, UNIT_NAME_STR, UNIT_SYM_STR )
-    
-    #if defined JM_UNITS_YT_AVAILABLE
-        #define JM_UNITS_INTERNAL_DEFINE_STRINGS_FOR_TEMPORAL_UNIT( \
-                UNIT_PLURAL, \
-                TRAITS, \
-                RATIO, \
-                UNIT_NAME_STR, \
-                UNIT_SYM_STR \
-            ) \
-        JM_UNITS_INTERNAL_DEFINE_MOST_STRINGS_FOR_TEMPORAL_UNIT( \
-            UNIT_PLURAL, \
-            TRAITS, \
-            RATIO, \
-            UNIT_NAME_STR, \
-            UNIT_SYM_STR \
-        ) \
-        template< typename T > using zepto##UNIT_PLURAL = _##UNIT_PLURAL##_unit< zepto_scale, T >; \
-        template< typename T > using zetta##UNIT_PLURAL = _##UNIT_PLURAL##_unit< zetta_scale, T >; \
-        template< typename T > using yocto##UNIT_PLURAL = _##UNIT_PLURAL##_unit< yocto_scale, T >; \
-        template< typename T > using yotta##UNIT_PLURAL = _##UNIT_PLURAL##_unit< yotta_scale, T >;
-    #elif defined JM_UNITS_ZT_AVAILABLE
-        #define JM_UNITS_INTERNAL_DEFINE_STRINGS_FOR_TEMPORAL_UNIT( \
-                UNIT_PLURAL, \
-                TRAITS, \
-                RATIO, \
-                UNIT_NAME_STR, \
-                UNIT_SYM_STR \
-            ) \
-        JM_UNITS_INTERNAL_DEFINE_MOST_STRINGS_FOR_TEMPORAL_UNIT( \
-            UNIT_PLURAL, \
-            TRAITS, \
-            RATIO, \
-            UNIT_NAME_STR, \
-            UNIT_SYM_STR \
-        ) \
-        template< typename T > using zepto##UNIT_PLURAL = _##UNIT_PLURAL##_unit< zepto_scale, T >; \
-        template< typename T > using zetta##UNIT_PLURAL = _##UNIT_PLURAL##_unit< zetta_scale, T >;
-    #else
-        #define JM_UNITS_INTERNAL_DEFINE_STRINGS_FOR_TEMPORAL_UNIT( \
-                UNIT_PLURAL, \
-                TRAITS, \
-                RATIO, \
-                UNIT_NAME_STR, \
-                UNIT_SYM_STR \
-            ) \
-        JM_UNITS_INTERNAL_DEFINE_MOST_STRINGS_FOR_TEMPORAL_UNIT( \
-            UNIT_PLURAL, \
-            TRAITS, \
-            RATIO, \
-            UNIT_NAME_STR, \
-            UNIT_SYM_STR \
-        )
-    #endif
-    
-    JM_UNITS_INTERNAL_DEFINE_STRINGS_FOR_TEMPORAL_UNIT( seconds, second_traits,         unit_scale, "seconds",   "s" )
-    JM_UNITS_INTERNAL_DEFINE_STRINGS_FOR_TEMPORAL_UNIT( minutes, minute_traits, std::ratio<   60 >, "minutes", "min" )
-    JM_UNITS_INTERNAL_DEFINE_STRINGS_FOR_TEMPORAL_UNIT(   hours,   hour_traits, std::ratio< 3600 >,   "hours",   "h" )
 } }
 
 
